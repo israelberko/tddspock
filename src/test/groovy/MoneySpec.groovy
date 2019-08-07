f7db6eca5e919c58667f35cc7ed229e286c8508c
israelb <israel.berko@cellwize.com>
2019-08-07 23:17:17 +0300

    quality for all

--- src/test/groovy/MoneySpec.groovy
+++ src/test/groovy/MoneySpec.groovy
@@ -6,32 +6,17 @@ class MoneySpec extends Specification {
 
     def "calculate 5\$ *2"() {
         given :
+
         Dollar five = new Dollar(5)
-        when :
-        five.times(2)
-        then:
-        five.amount == 10
+        Dollar six = new Dollar(5)
 
-        when :
-        five.times(3)
-        then:
-        five.amount == 15
+        expect:
+        five != new Dollar(5)
+        five != six
+
 
 
     }
 
-    def "calculate 5\$ *2 with Data Tables"() {
-        given :
-            Dollar five = new Dollar(5)
-        when :
-            five.times(multiplier)
-        then:
-            five.amount == result
-
-        where:
-            multiplier || result
-            2  || 10
-            3  || 15
-    }
 
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -9,11 +11,19 @@ public class MoneyTest {
 
     @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-        five.times(3);
-        assertEquals(15, five.amount);
 
+        Dollar five= new Dollar(5);
+        Dollar product= five.times(2);
+        assertEquals(10, product.amount);
+        product= five.times(3);
+        assertEquals(15, product.amount);
+
+
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
