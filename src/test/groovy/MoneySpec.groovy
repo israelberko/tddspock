22908f23a25e9d46a2b6145ae8633ceda58f6105
israelb <israel.berko@cellwize.com>
2019-08-07 22:19:01 +0300

    degenerate object basic

--- src/test/groovy/MoneySpec.groovy
+++ src/test/groovy/MoneySpec.groovy
@@ -6,11 +6,32 @@ class MoneySpec extends Specification {
 
     def "calculate 5\$ *2"() {
         given :
+        Dollar five = new Dollar(5)
+        when :
+        five.times(2)
+        then:
+        five.amount == 10
+
+        when :
+        five.times(3)
+        then:
+        five.amount == 15
+
+
+    }
+
+    def "calculate 5\$ *2 with Data Tables"() {
+        given :
             Dollar five = new Dollar(5)
         when :
-            five.times(2)
+            five.times(multiplier)
         then:
-            five.amount == 10
+            five.amount == result
+
+        where:
+            multiplier || result
+            2  || 10
+            3  || 15
     }
 
 }
