60b80c35444939686043d23b41c96704c6a75cfc
israelb <israel.berko@cellwize.com>
2019-08-07 21:29:06 +0300

    calculate 5 USD *2

---
+++ src/test/groovy/MoneySpec.groovy
@@ -0,0 +1,16 @@
+import spock.lang.*
+
+
+
+class MoneySpec extends Specification {
+
+    def "calculate 5\$ *2"() {
+        given :
+            Dollar five = new Dollar(5)
+        when :
+            five.times(2)
+        expect:
+            five.amount == 10
+    }
+
+}
