f610ab18529ee3679dd067e213e8d235bd1e7fb8
israelb <israel.berko@cellwize.com>
2019-08-20 08:51:10 +0300

    apples and oranges ends

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,7 +2,7 @@ public class Money {
     protected int amount;
 
     public boolean equals(Object object) {
-        Money dollar= (Money) object;
-        return amount == dollar.amount;
+        Money money= (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
     }
 }
