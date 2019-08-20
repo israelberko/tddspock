d5eb0f4944e902c26b953ecdc0cd821c706cc7bc
israelb <israel.berko@cellwize.com>
2019-08-20 13:31:14 +0300

    addition - finally -2

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
- class Money {
+ class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -18,8 +18,13 @@
         return new Money(amount * multiplier, currency);
     }
 
+     Expression plus(Money addend) {
+         return new Money(amount + addend.amount, currency);
+     }
 
-    String currency() {
+
+
+     String currency() {
         return currency;
     }
 
